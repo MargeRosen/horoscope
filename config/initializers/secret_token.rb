7bf871b0aab9fe3657f872c5f9b8eaef14f81049
Marge Rosen <marge@masterclassu.com>
2013-07-29 09:29:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horoscope::Application.config.secret_token = 'ffac5644c53e6bbe45a217cfbc3004067c9e72eae4004b1bce7b2502764a8b3f9865e514eb72607a724e8330cf3f203a48bab075268f70a49addc85251e329fa'
