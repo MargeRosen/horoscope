86d8e7b095d43f9d9ca3484eec3218115104d962
Marge Rosen <marge@masterclassu.com>
2013-08-10 08:11:19 -0700

    update readme

--- spec/integration/creating_horoscopes_spec.rb
+++ spec/integration/creating_horoscopes_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
-feature 'Creating Your Sign' do
+feature 'Your Sign' do
   scenario "can create your sign" do
     visit '/'
     click_link 'Enter your birthdate'
-    fill_in 'Birthdate', :with => '12071971'
+    #fill_in 'Birthdate', :with => '12071971'
+    select birthdate.year.to_s, :from => 'birthdate_1i'
+    select Date::MONTHNAMES[birthdate.month], :from => 'birthdate_2i'
+    select birthdate.date.to_s :from => 'birthdate_3i'
     click_button 'Find your sign'
     page.should have_content('Sign has been created.')
   end
